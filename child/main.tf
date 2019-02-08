96cbca61d7444dec55f1ca3ea353829d35ce6b50
suraj <suraj.bodade@zemosolabs.com>
2019-02-08 14:39:59 +0530

    sample dev_env infrastructure terraform files

---
+++ child/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "sunrays_aws_terraform" {
+    bucket = "sunrays-aws-terraform-bucket"
+}
---
+++ dev_environment.tf
@@ -0,0 +1,41 @@
+# This is sample configuration file for testing various elements of terraform resources and modules
+
+provider "aws" {
+  region = "ap-south-1"
+  shared_credentials_file = "/home/zadmin/.aws/credentials"
+  profile = "devops"
+}
+module "ec2_cluster" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "1.12.0"
+
+  name                   = "my-cluster"
+  instance_count         = "2"
+  ami                    = "ami-04ea996e7a3e7ad6b" # UBUNTU 16.04
+  instance_type          = "t2.micro"
+  key_name               = "sunrays_aws_terraform"
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "sunrays_test_instance" {
+    ami = "ami-04ea996e7a3e7ad6b"
+    instance_type = "t2.micro"
+
+    provisioner "remote-exec" {
+      script = "test.sh"
+    }
+}
+
+resource "aws_sns_topic" "development" {
+    name = "development"
+}
+
+module "child" {
+  source = "./child"
+}
